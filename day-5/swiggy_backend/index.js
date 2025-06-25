1bf359b7131f462234c852508ca3158374bf1064
Samarth <officialdevelopersamarth@gmail.com>
2025-06-25 11:51:37 +0530

    5th lecture swiggy backend

---
+++ day-5/swiggy_backend/index.js
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+const app = express();
+import restaurantRoutes from './routes/restaurant.routes.js'
+
+mongoose.connect('mongodb://127.0.0.1:27017/swiggy_backend')
+.then(()=>{ console.log("DB CONNECTED SUCCESSFULLY")  })
+.catch((ERR)=>{ console.log("DB FAILED" , ERR) })
+
+app.use(express.json())// body
+
+app.get('/' , (req,res)=>{
+    res.send("welcome to root route")
+})
+
+restaurantRoutes(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT , ()=>{
+    console.log(`server connected at port: ${PORT}`);
+})
+
+// samarthvohraindia
+// HiwWJyrzZJ0qBuVl
+
+
+
+
+
+
+
+
---
+++ day-5/swiggy_backend/routes/restaurant.routes.js
@@ -0,0 +1,12 @@
+import { createRestaurant, fetchRestaurant } from "../controllers/restaurant.controller.js";
+
+function restaurantRoutes(app){
+    app.get('/api/restaurants' , fetchRestaurant),
+    app.post('/api/restaurant' ,createRestaurant)
+}
+
+export default restaurantRoutes;
+
+
+
+
